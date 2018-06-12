2d0b18c3f43efd80a685346a77f27e983b3e716c
Denis <den.kolesov@gmail.com>
2018-06-12 18:53:58 +0300

    Initial commit

---
+++ AsyncReturnTypes.xUnit/AsyncReturnTypeManagerTest.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+using AsyncReturnTypeLibrary;
+using System.Threading.Tasks;
+
+namespace AsyncReturnTypes.xUnit
+{
+    public class AsyncReturnTypeManagerTest 
+    {
+        private AsyncReturnTypeManager _returnTypeMgr;
+
+        public AsyncReturnTypeManagerTest()
+        {
+            _returnTypeMgr = new AsyncReturnTypeManager();
+        }
+        
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
